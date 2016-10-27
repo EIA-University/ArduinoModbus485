5ae330500d8d4b3c9ca6f7a17b7c5fb6c808c40f
Jurkomason <jurkorreo@gmail.com>
2016-10-27 13:37:41 -0500

    Add files via upload

---
+++ SistemaOperativoSerialRead.ino
@@ -0,0 +1,23 @@
+int Boton(8);
+int Led(11);
+int dato=0;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(Boton,INPUT);
+   pinMode(Led,OUTPUT);
+}
+
+void loop() {
+ dato=Serial.read();
+ Serial.write(dato);
+ if(dato!='0'){
+  digitalWrite(Led,HIGH);
+ 
+ }
+ else{
+  digitalWrite(Led,LOW);
+ }
+ dato='0';
+  
+}
---
+++ SistemaOperativoSerialWrtite.ino.ino
@@ -0,0 +1,32 @@
+const int buttonPin = 2;    
+const int ledPin =  13;      
+
+// variables will change:
+int buttonState = 0;         // variable for reading the pushbutton status
+
+void setup() {
+  Serial.begin(9600);
+  // initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+
+  // check if the pushbutton is pressed.
+  // if it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    // turn LED on:
+    Serial.write('1');
+    
+    
+  } else {
+    // turn LED off:
+     Serial.write('0');
+  
+  }
+  delay(1000);
+}
